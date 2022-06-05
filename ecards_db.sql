dc3ecd5cb3b42f1b741fdb525d8bbfc7f70709d0
Evgeni-Georgiev <evgenigg2000@gmail.com>
2022-06-05 12:07:15 +0300

    Modified Database. Added column for resetting passwords.

--- ecards_db.sql
+++ ecards_db.sql
@@ -1,26 +1,30 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jun 01, 2022 at 10:39 AM
--- Server version: 5.7.24
--- PHP Version: 7.4.1
+-- Host: localhost:8889
+-- Generation Time: Jun 05, 2022 at 00:00 AM
+-- Server version: 5.6.38
+-- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `ecards_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reset_password_codes`
+--
+
+CREATE TABLE `reset_password_codes` (
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,31 +46,38 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
+
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `status`, `email_verification_link`, `email_verified_at`) VALUES
-(12, 'Ivan', 'ivan@gmail.com', '$2y$10$LTyh3J/zwz7bwiLraZKzx.spIVoNaxdFP.c5geCyRVt3g6njrCNpe', 0, 'd99c9093443e7bfc295ac857adcfa11f8591', NULL);
+(1, 'Ivan', 'ivan@gmail.com', '$2y$10$LTyh3J/zwz7bwiLraZKzx.spIVoNaxdFP.c5geCyRVt3g6njrCNpe', 0, 'd99c9093443e7bfc295ac857adcfa11f8591', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `reset_password_codes`
+--
+ALTER TABLE `reset_password_codes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `reset_password_codes`
+--
+ALTER TABLE `reset_password_codes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
