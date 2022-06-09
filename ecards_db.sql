ded99bf128eb22eac29b95941b3dd0bf9a6f05b8
Evgeni-Georgiev <evgenigg2000@gmail.com>
2022-06-09 16:39:40 +0300

    Added modified database.

--- ecards_db.sql
+++ ecards_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 06, 2022 at 09:27 AM
+-- Generation Time: Jun 09, 2022 at 03:36 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -29,11 +29,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cards` (
-                         `id` int(11) NOT NULL,
-                         `code` varchar(255) NOT NULL,
-                         `image` varchar(280) NOT NULL,
-                         `description` varchar(255) DEFAULT NULL
+  `card_id` int(11) NOT NULL,
+  `users_id` int(11) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cards`
+--
 
 -- --------------------------------------------------------
 
@@ -42,17 +47,29 @@ CREATE TABLE `cards` (
 --
 
 CREATE TABLE `reset_password_codes` (
-                                        `id` int(11) NOT NULL,
-                                        `code` varchar(255) NOT NULL,
-                                        `email` varchar(255) NOT NULL
+  `id` int(11) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `send_card`
+--
+
+CREATE TABLE `send_card` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `card_id` int(11) DEFAULT NULL,
+  `code` varchar(255) DEFAULT NULL,
+  `description_card` varchar(255) DEFAULT NULL,
+  `recipient_email` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `reset_password_codes`
+-- Dumping data for table `send_card`
 --
-
-INSERT INTO `reset_password_codes` (`id`, `code`, `email`) VALUES
-    (1, '1629da570e76f6', 'aleksandarrusakov@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -61,23 +78,19 @@ INSERT INTO `reset_password_codes` (`id`, `code`, `email`) VALUES
 --
 
 CREATE TABLE `users` (
-                         `id` int(255) NOT NULL,
-                         `username` varchar(255) NOT NULL,
-                         `email` varchar(255) NOT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `status` int(11) DEFAULT '0',
-                         `email_verification_link` varchar(255) DEFAULT NULL,
-                         `email_verified_at` timestamp NULL DEFAULT NULL
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) DEFAULT '0',
+  `email_verification_link` varchar(255) DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `status`, `email_verification_link`, `email_verified_at`) VALUES
-                                                                                                                          (12, 'Ivan', 'ivan@gmail.com', '$2y$10$LTyh3J/zwz7bwiLraZKzx.spIVoNaxdFP.c5geCyRVt3g6njrCNpe', 0, 'd99c9093443e7bfc295ac857adcfa11f8591', NULL),
-                                                                                                                          (31, 'Aleksandar', 'aleksandar@rusakov.com', '$2y$12$WBLWwIbAWtYwFyWgegKS5ulTn5MqgIWO5/POMmcT6U0QtqTcZ4q9W', 0, '0ed5f0ab11a31b9566cad4f8a156df967270', NULL),
-                                                                                                                          (32, 'Aleksandar1', 'aleksandarrusakov@gmail.com', '$2y$12$Aj552Ht4u.qLO.56iDUHW.rfSujFjC1lLYtg793cr5y8EtswwYS8q', 0, '9445e1c987c3d657eb69e31418f4d9b86064', '2022-06-06 05:54:30');
 
 --
 -- Indexes for dumped tables
@@ -87,19 +100,28 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `status`, `email_ver
 -- Indexes for table `cards`
 --
 ALTER TABLE `cards`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`card_id`),
+  ADD KEY `users_id` (`users_id`);
 
 --
 -- Indexes for table `reset_password_codes`
 --
 ALTER TABLE `reset_password_codes`
-    ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `send_card`
+--
+ALTER TABLE `send_card`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `card_id` (`card_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-    ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -107,16 +129,45 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `cards`
+--
+ALTER TABLE `cards`
+  MODIFY `card_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `reset_password_codes`
 --
 ALTER TABLE `reset_password_codes`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `send_card`
+--
+ALTER TABLE `send_card`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-    MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `cards_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `send_card`
+--
+ALTER TABLE `send_card`
+  ADD CONSTRAINT `send_card_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `send_card_ibfk_2` FOREIGN KEY (`card_id`) REFERENCES `cards` (`card_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
